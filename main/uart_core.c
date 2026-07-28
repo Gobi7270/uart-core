8cf5a8ed4d81fb7525b15542e4405de13c474b84
gobi7270 <gobi7270@gmail.com>
2026-07-28 22:46:47 +0530

    Sending byte implementation

--- main/uart_core.c
+++ main/uart_core.c
@@ -2,15 +2,33 @@
 // GPIO_ENABLE_REG GPIO 0-31 output enable register 0x3FF44020 R/W
 // GPIO_ENABLE_W1TS_REG GPIO 0-31 output enable register_W1TS 0x3FF44024 WO --> Set Reg
 // GPIO_ENABLE_W1TC_REG GPIO 0-31 output enable register_W1TC 0x3FF44028 WO --> Clear Reg
-// GPIO_OUT_W1TC_REG
-// GPIO_OUT_W1TS_REG
+// GPIO_OUT_W1TC_REG 0-31
+// GPIO_OUT_W1TS_REG 0-31
+
+/* 
+GPIO_ENABLE
+        │
+        ├── OUTPUT  (driver ON)
+        └── INPUT   (driver OFF)
+
+GPIO_OUT
+        │
+        ├── HIGH
+        └── LOW 
+*/
 
 #include "soc/soc.h"
 #include "soc/gpio_reg.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 //global configs
 #define SW_UART_TX_PIN 27
 #define SW_UART_RX_PIN 26
+#define SW_UART_BIT_TIME_MS 1000
+#define HIGH 1
+#define LOW 0
+#define UART_DATA_FRAME 8
 
 
 void SoftUART_Init()
@@ -29,4 +47,29 @@ void SoftUART_Init()
 void app_main(void)
 {
     SoftUART_Init();
+}
+
+void SoftUART_SendBit(bool bit)
+{
+    if (bit)
+    {
+        REG_WRITE(GPIO_OUT_W1TS_REG,(1 << SW_UART_TX_PIN));
+    }
+    else
+    {
+        REG_WRITE(GPIO_OUT_W1TC_REG,(1 << SW_UART_TX_PIN));
+    }
+    vTaskDelay(SW_UART_BIT_TIME_MS/portTICK_PERIOD_MS);
+}
+
+void SoftUART_SendByte(uint8_t data)
+{
+    // Start bit LOW
+    SoftUART_SendBit(LOW);
+    for(uint8_t pos = 0; pos < UART_DATA_FRAME; pos++)
+    {
+        SoftUART_SendBit((data & (1 << pos)));
+    }
+    // Stop bit HIGH
+    SoftUART_SendBit(HIGH);
 }
--- main/uart_core.h
+++ main/uart_core.h
@@ -1,3 +1,6 @@
 // Function declarations
+#include <stdint.h>
 
 void SoftwareUART_Init();
+void SoftUART_SendBit(bool bit);
+void SoftUART_SendByte(uint8_t data);
